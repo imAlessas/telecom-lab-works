0d12fe030a194135817ef1d769fa6a6b17095306
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2023-12-18 11:41:20 +0200

    added lab-6 script

---
+++ src/lab-6/PD6_QAM16.m
@@ -0,0 +1,56 @@
+clc, clear, format compact
+
+N = 1e6;                    % Number of bits
+binDt = randi(2,1,N) - 1;   % Generate integer numbers in range [1, 2]
+                            % Subtract 1 to format data in range [0, 1]
+SNR = -5;                   % Signal to Noise ratio, dB
+
+% ----------------------------------------------------------------------
+% Specify QAM-16 constellation as column vector
+% Each row represents coordinates of a signal in constellation with
+% code matching the number of the row (-1 due to Matlab indexing specifics)
+mapQAM16 = [-1-1i; -1-3i; -1+1i; -1+3i;
+            -3-1i; -3-3i; -3+1i; -3+3i;
+            +1-1i; +1-3i; +1+1i; +1+3i;
+            +3-1i; +3-3i; +3+1i; +3+3i];
+
+% ----------------------------------------------------------------------             
+% Format binary data as symbols consisting of 4 bits per symbol
+% Use 'reshape' command to create 4xN/4 matrix
+symDt = reshape(binDt, 4, N/4);
+
+% Each column of this matrix represents a QAM-16 symbol (4 bits)
+% Convert this binary notation of symbols into decimal format
+% Note: The first row denotes the most significant bit
+symDt = 2.^(3:-1:0) * symDt; 
+
+% Now each symbol is represented in decimal form in range from 0 to 15
+
+
+% ----------------------------------------------------------------------             
+% Perform modulation by mapping integer symbols to corresponding QAM-16 signals
+% Furthermore, add noise with specified SNR by using "awgn()" function.
+symMod = mapQAM16(symDt + 1);     % Add +1 to each symbol to account for Matlab indexing
+symNoi = awgn(symMod, SNR);
+
+% Plot resulting signals:
+%   * Blue dots - noisy signal
+%   * Red crosses - ideal signals
+figure(1);
+plot(symNoi, 'b.');
+hold on, plot(mapQAM16, 'rx'), hold off;
+xlim([-4, 4]), ylim([-4, 4]);
+xlabel('Real Part'), ylabel('Immaginary Part');
+axis square;
+
+% ----------------------------------------------------------------------             
+% Perform demodulation by finding closest ideal signal from constellation and
+% convert decimal symbols into binary codewords. This script is provided.
+% Feel free to study its contents and evaluate its operation.
+demodData
+
+% ----------------------------------------------------------------------
+% Count number of errors by comparing vectors binDt and binDmd.
+% Use '~=' logical operator for comparison and count number of mismatches
+numErr = sum(binDmd ~= binDt)
+BERval = numErr/N          % Number of errors ratio to total number of bits N
---
+++ src/lab-6/demodData.m
@@ -0,0 +1,29 @@
+% The script determines the binary sequence of each received QAM-16 symbol
+% This is achieved by comparing absolute distance between received symbol
+%   and every possible QAM-16 constellation signal
+% Note: The resulting binary data is stored in binDmd vector-row
+
+% This approach requires replicating column of received symbols to create
+%   a matrix with dimensions N/4x16. "repmat()" function is used to do so.
+symMtx = repmat(symNoi, 1, 16);
+
+% Likewise, convert constellation vector into row and replicate it to match
+%   dimensions of symMtx matrix.
+mapMtx = repmat(mapQAM16.', N/4, 1);     % Note ".'" transpose for complex values!
+
+% Now calculate distances to each QAM-16 symbol for each row.
+dstMtx = abs(symMtx - mapMtx);
+
+% Find minimum distance in each row and determine the column number of this minimum
+% Find minimums in rows (dimension 2)
+[dstMin, symDmd] = min(dstMtx, [], 2);
+
+% These minimum values are decimal representations (with added +1) of binary data
+% Perform BIN -> DEC conversion
+binDmd = dec2bin(symDmd - 1, 4);         % Subtract -1 to set minimum symbol value to 0
+binDmd = (binDmd == '1')';
+
+% The output of the dec2bin function is a TEXT string.
+% Perform logical comparison to obtain numerical values.
+binDmd = binDmd(:)';            % Transpose matrix to dimensions 4xN/4
+% binDmd =             % Unwrap matrix into single column, convert to row
