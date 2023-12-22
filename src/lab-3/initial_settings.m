51a0e9aaee187ad11057bb587e6877c8fe8da666
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2023-12-22 16:33:25 +0200

    minor changes on initial_settings.m

--- src/lab-3/initial_settings.m
+++ src/lab-3/initial_settings.m
@@ -3,5 +3,5 @@ f0 = 10; % carrier frequecy [Hz]
 N = 10000; % number of symbols
 binary_sequence = randi(2, 1, N) - 1; % random sequence of 1 and 0
 % binary_sequence = [0 0 1 0 1 0 0 0 1 1 1 1 0 1 1 ];
-SNR = 7; % Db
-draw_plots = 0;
+SNR = 5; % Db
+draw_plots = 1;
