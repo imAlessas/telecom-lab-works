c90dde52f28bb8ca4d279426030ac8d438530e4d
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2023-12-22 20:51:41 +0200

    minor changes on lab-3\initial-settings.m
    Changes made in view of lab-5\report.tex

--- src/lab-3/initial_settings.m
+++ src/lab-3/initial_settings.m
@@ -1,6 +1,6 @@
 T = 1; % symbol duration [seconds]
 f0 = 10; % carrier frequecy [Hz]
-N = 10000; % number of symbols
+N = 15; % number of symbols
 binary_sequence = randi(2, 1, N) - 1; % random sequence of 1 and 0
 % binary_sequence = [0 0 1 0 1 0 0 0 1 1 1 1 0 1 1 ];
 SNR = 5; % Db
